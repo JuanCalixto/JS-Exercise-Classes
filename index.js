f754fef5b7ee8032481465910771db7dc1e4996f
JuanCalixto <juancalixtomorales@gmail.com>
2020-12-10 21:18:17 -0600

    Car class task 2 complete and tests pass

--- index.js
+++ index.js
@@ -84,8 +84,29 @@ class Airplane {
   */
   
  class Car {
-    
+  constructor(model, milesPerGallon) {
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
   }
+
+  fill(gallons) {
+    return this.tank += gallons
+  }
+
+  drive(distance) {
+    let maxDistance = this.tank * this.milesPerGallon;
+    if(maxDistance < distance){
+      this.odometer += maxDistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    } else {
+        this.odometer += distance   
+        this.tank = this.tank - (distance / this.milesPerGallon)
+    }
+  } 
+}
   
   /*
     TASK 3
